5f5d32c4b97379d5e6a55f98e31a8b368377d99c
buzilong <bzlwork@126.com>
2017-03-05 19:23:00 +0800

    add log config

--- springBootSample/src/main/java/com/buzl/springboot/controller/DemoController.java
+++ springBootSample/src/main/java/com/buzl/springboot/controller/DemoController.java
@@ -2,6 +2,7 @@ package com.buzl.springboot.controller;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,13 @@ import com.buzl.springboot.service.DemoService;
 
 @RestController
 public class DemoController {
+	private static final Logger LOGGER = Logger.getLogger(DemoController.class);
 	@Autowired
 	private DemoService demoService;
 
 	@RequestMapping("/likeName")
 	public List<Demo> likeName(String name) {
-		System.out.println("Enter In Controller");
+		LOGGER.info("Enter in controller");
 		return demoService.likeName(name);
 	}
 }
