625737b25b66fe95a297effb7edcc5806dba6353
bzlwork <bzlwork@126.com>
2019-09-13 14:43:56 +0800

    add static ioc code， fix mysql time issue

--- springBoot_integration/src/main/java/com/buzl/springboot/controller/TestController.java
+++ springBoot_integration/src/main/java/com/buzl/springboot/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+
 public class TestController {
 
 	private RestTemplate restTemplate = new RestTemplate();
