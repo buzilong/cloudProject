e27415294a155d8bd4916b0163edca593be820c4
bzlwork <bzlwork@126.com>
2019-09-13 14:33:51 +0800

    format code

--- springBoot_integration/src/main/java/com/buzl/springboot/Application.java
+++ springBoot_integration/src/main/java/com/buzl/springboot/Application.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource(value = { "config/application.properties" })
+@PropertySource(value = {"config/application.properties"})
 @MapperScan("com.buzl.springboot.mapper*")
-public class Application{
-	
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
